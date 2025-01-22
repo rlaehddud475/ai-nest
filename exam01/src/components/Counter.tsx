0b666c7804b0c7102471b158b18277538fe7e820
rlaehddud475 <6070kjh475@naver.com>
2025-01-22 13:20:28 +0900

    .

--- exam01/src/components/Counter.tsx
+++ exam01/src/components/Counter.tsx
@@ -4,11 +4,11 @@ const Counter = () => {
   const [number, setNumber] = useState<number>(1)
 
   return (
-    <div>
+    <>
       <h1>{number}</h1>
       <button onClick={() => setNumber(number + 1)}>+1</button>
       <button onClick={() => setNumber(number - 1)}>-1</button>
-    </div>
+    </>
   )
 }
 
