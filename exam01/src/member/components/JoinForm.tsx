bbff86a389cd63916b071b2807a5302a4a30d202
rlaehddud475 <6070kjh475@naver.com>
2025-02-03 10:35:55 +0900

    .

--- exam01/src/member/components/JoinForm.tsx
+++ exam01/src/member/components/JoinForm.tsx
@@ -1,30 +1,69 @@
-const JoinForm = (): React.ReactNode => {
+'use client'
+import { FaRegSquareCheck, FaSquareCheck } from 'react-icons/fa6'
+
+const JoinForm = ({
+  onSubmit,
+  onChange,
+  onToggle,
+  form,
+  errors,
+}): React.ReactNode => {
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <dl>
         <dt>이메일</dt>
         <dd>
-          <input type="text" />
+          <input
+            type="text"
+            name="email"
+            value={form.email}
+            onChange={onChange}
+          />
+          {errors?.email && errors.email.map((m) => <div key={m}>{m}</div>)}
         </dd>
       </dl>
       <dl>
         <dt>비밀번호</dt>
         <dd>
-          <input type="password" />
+          <input
+            type="password"
+            name="password"
+            value={form.password}
+            onChange={onChange}
+          />
+          {errors?.password &&
+            errors.password.map((m) => <div key={m}>{m}</div>)}
         </dd>
       </dl>
       <dl>
         <dt>비밀번호 확인</dt>
         <dd>
-          <input type="password" />
+          <input
+            type="password"
+            name="confirmPassword"
+            value={form.confirmPassword}
+            onChange={onChange}
+          />
+          {errors?.confirmPassword &&
+            errors.confirmPassword.map((m) => <div key={m}>{m}</div>)}
         </dd>
       </dl>
       <dl>
         <dt>회원명</dt>
         <dd>
-          <input type="text" />
+          <input
+            type="text"
+            name="name"
+            value={form.name}
+            onChange={onChange}
+          />
+          {errors?.name && errors.name.map((m) => <div key={m}>{m}</div>)}
         </dd>
       </dl>
+      <div onClick={onToggle}>
+        {form.agree ? <FaSquareCheck /> : <FaRegSquareCheck />} 회원가입 약관에
+        동의합니다.
+      </div>
       <button type="submit">가입하기</button>
     </form>
   )
--- exam01/src/member/containers/JoinContainer.tsx
+++ exam01/src/member/containers/JoinContainer.tsx
@@ -1,7 +1,81 @@
+'use client'
+import { useState } from 'react'
 import JoinForm from '../components/JoinForm'
 
+type FormType = {
+  email?: string
+  password?: string
+  confirmPassword?: string
+  name?: string
+  agree?: boolean
+}
+
 const JoinContainer = (): React.ReactNode => {
-  return <JoinForm />
+  const [form, setForm] = useState<FormType>({})
+  const [errors, setErrors] = useState({})
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleToggle = () => {
+    setForm({ ...form, agree: !Boolean(form.agree) })
+  }
+
+  /**
+   * 양식 제출시 처리 함수
+   */
+  const onSubmit = (e) => {
+    // form 태그의 기본 동작 차단
+    e.preventDefault()
+
+    /**
+     * 양식 검증
+     * 1) 필수 항목 검증
+     * 2) 비밀번호 자리수
+     * 3) 비밀번호 확인과 일치 여부
+     * 4) 서버 처리 요청
+     * 5) 가입 완료 후 후속 처리
+     */
+
+    const _errors = {}
+    let hasErrors = false
+    // 1) 필수 항목 검증 S
+    const requiredFields = {
+      email: '이메일을 입력하세요.',
+      password: '비밀번호를 입력하세요.',
+      confirmPassword: '비밀번호를 확인하세요.',
+      name: '이름을 입력하세요.',
+    }
+
+    for (const [field, msg] of Object.entries(requiredFields)) {
+      if (!form[field] || !form[field].trim()) {
+        // 필수 항목 누락이 된 경우
+        _errors[field] = _errors[field] ?? []
+        _errors[field].push(msg)
+        hasErrors = true
+      }
+    }
+
+    // 1) 필수 항목 검증 E
+    setErrors(_errors)
+
+    if (!hasErrors) {
+      // 검증 성공 : 서버쪽에 처리 요청
+    }
+  }
+
+  return (
+    <>
+      <JoinForm
+        onSubmit={onSubmit}
+        onChange={handleChange}
+        onToggle={handleToggle}
+        errors={errors}
+        form={form}
+      />
+    </>
+  )
 }
 
 export default JoinContainer
