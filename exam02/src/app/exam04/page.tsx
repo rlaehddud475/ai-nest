82144db4c87dd795f923e00f0e9fb5a3b66798b5
rlaehddud475 <6070kjh475@naver.com>
2025-02-05 12:04:59 +0900

    .

---
+++ exam02/src/app/exam04/page.tsx
@@ -0,0 +1,12 @@
+import './box.scss'
+const Exam04Page = () => {
+  return (
+    <div className="box-group">
+      <div className="box red"></div>
+      <div className="box blue"></div>
+      <div className="box orange"></div>
+      <div className="box gray"></div>
+    </div>
+  )
+}
+export default Exam04Page
